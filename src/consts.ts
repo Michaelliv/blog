91ab8f240e69ca83fefc1fb4ccecd7b1fcccea57
Michael Livshits <livsmichael@gmail.com>
2025-11-28 00:03:35 +0200

    Initial commit: /dev/michael blog

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = '/dev/michael';
+export const SITE_DESCRIPTION = 'Build, Break, Repeat';
