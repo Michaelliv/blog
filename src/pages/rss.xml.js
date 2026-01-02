d7a76f12882db852e78ad86b046871e1dce808ec
Michael Livshits <livsmichael@gmail.com>
2026-01-02 15:29:23 +0200

    Strip MDX syntax from RSS content

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -3,6 +3,15 @@ import rss from '@astrojs/rss';
 import { marked } from 'marked';
 import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
 
+function stripMdx(content) {
+	return content
+		.replace(/^import\s+.*$/gm, '') // Remove import statements
+		.replace(/<[A-Z][a-zA-Z]*\s*[^>]*\/>/g, '') // Remove self-closing components <Component />
+		.replace(/<[A-Z][a-zA-Z]*[^>]*>[\s\S]*?<\/[A-Z][a-zA-Z]*>/g, '') // Remove component blocks <Component>...</Component>
+		.replace(/\n{3,}/g, '\n\n') // Clean up excess newlines
+		.trim();
+}
+
 export async function GET(context) {
 	const posts = (await getCollection('blog')).sort(
 		(a, b) => b.data.pubDate.valueOf() - a.data.pubDate.valueOf()
@@ -19,7 +28,7 @@ export async function GET(context) {
 			link: `/blog/${post.id}/`,
 			author: 'Michael Livshits',
 			categories: post.data.tags || [],
-			content: marked.parse(post.body || ''),
+			content: marked.parse(stripMdx(post.body || '')),
 		})),
 	});
 }
